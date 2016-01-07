2e7c6b3f711ab9c210012340b1530c62178ab14f
Daniel Robinson <dan.rob.xor@gmail.com>
2016-01-07 18:45:52 +0000

    update to file

--- interface.go
+++ interface.go
@@ -2,15 +2,15 @@ package vada
 
 import(
 	. "github.com/robsix/json"
-	"io"
 	"net/http"
+	"mime/multipart"
 )
 
 type VadaClient interface {
 	CreateBucket(bucketKey string, policyKey bucketPolicy) (*Json, error)
 	GetBucketDetails(bucketKey string) (*Json, error)
 	GetSupportedFormats() (*Json, error)
-	UploadFile(objectKey string, bucketKey string, objectReader io.Reader) (*Json, error)
+	UploadFile(objectKey string, bucketKey string, file multipart.File) (*Json, error)
 	RegisterFile(b64Urn string) (*Json, error)
 	GetDocumentInfo(b64Urn string, guid string) (*Json, error)
 	GetSheetItem(b64UrnAndItemPath string) (*http.Response, error)
--- uploadfile.go
+++ uploadfile.go
@@ -3,11 +3,11 @@ package vada
 import (
 	"git.autodesk.com/typhoon/stormfront/src/server/Godeps/_workspace/src/code.google.com/p/go-uuid/uuid"
 	sj "git.autodesk.com/typhoon/stormfront/src/server/Godeps/_workspace/src/github.com/robsix/json"
-	"io"
 	"net/url"
+	"mime/multipart"
 )
 
-func uploadFile(host string, objectKey string, bucketKey string, objectReader io.Reader, accessToken string) (ret *sj.Json, err error) {
+func uploadFile(host string, objectKey string, bucketKey string, file multipart.File, accessToken string) (ret *sj.Json, err error) {
 	url, err := url.Parse(host + "/oss/v2/buckets/"+bucketKey+"/objects/")
 	if err != nil {
 		return nil, err
@@ -15,7 +15,7 @@ func uploadFile(host string, objectKey string, bucketKey string, objectReader io
 
 	url.Path += uuid.New() + objectKey
 
-	req, err := newRequest("PUT", url.String(), objectReader, accessToken, "application/octet-stream")
+	req, err := newRequest("PUT", url.String(), file, accessToken, "application/octet-stream")
 	if err != nil {
 		return nil, err
 	}
